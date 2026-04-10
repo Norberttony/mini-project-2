1b5b81b8f15ba8e9ac7fa901c7ac68caa2fd42cf
Norbert Krajewski <norberttony@icloud.com>
2026-04-10 00:39:45 -0400

    custom test case for multiple methods

--- tests/custom.rkt
+++ tests/custom.rkt
@@ -1,1 +1,21 @@
 #lang plait
+
+(require
+  "../main.rkt"
+  "../values.rkt")
+
+; Allow any number of methods.
+(test
+ (run `{let1 {obj {object
+                   {method add1 {lam x {+ x 1}}}
+                   {method add2 {lam x {+ x 2}}}}}
+             {send obj "add2" 5}}
+      ) (numV 7))
+
+; Allow fields
+(test
+ (run `{let1 {obj {object
+                   {field myVal 3}
+                   {method get1 {lam _ myVal}}}}
+             {send obj "get1"}}
+      ) (numV 3))
